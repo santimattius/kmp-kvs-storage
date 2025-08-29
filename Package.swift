f97299a68eb60c1706157100a3455f204a236706
Santiago Mattiauda <santimattius@gmail.com>
2025-08-28 21:39:32 -0300

    [version] Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "KvsStorage",
+    platforms: [
+        .iOS(.v14),
+    ],
+    products: [
+        .library(name: "KvsStorage", targets: ["KvsStorage"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "KvsStorage",
+            url: "https://github.com/santimattius/kmp-kvs-storage/releases/download/1.0.0-ALPHA01/KvsStorage.xcframework.zip",
+            checksum:"c21bc919df2b7f9dd05a281915565b2c8e558dc8af33485ff8a004d4ce520fe4")
+    ]
+)
+
