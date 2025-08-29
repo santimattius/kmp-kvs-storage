fef86456a1affb01b24640374a4e45f894f13715
Santiago Mattiauda <santimattius@gmail.com>
2025-08-29 19:07:02 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KvsStorage",
-            url: "https://github.com/santimattius/kmp-kvs-storage/releases/download/1.0.0-ALPHA01/KvsStorage.xcframework.zip",
+            url: "https://github.com/santimattius/kmp-kvs-storage/releases/download/1.0.0-ALPHA02/KvsStorage.xcframework.zip",
             checksum:"c21bc919df2b7f9dd05a281915565b2c8e558dc8af33485ff8a004d4ce520fe4")
     ]
 )
