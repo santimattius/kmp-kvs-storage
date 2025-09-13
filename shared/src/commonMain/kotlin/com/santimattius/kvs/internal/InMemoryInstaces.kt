67eee9d8ecf938484eaa08fb8a0693ee72571e78
Santiago Mattiauda <santimattius@gmail.com>
2025-09-12 21:38:35 -0300

    [feature] InMemoryKvs.kt for testing (#2)
    * [feature] InMemoryKvs.kt for testing
    
    * [feature] InMemoryKvs.kt for testing
    
    * [feature] Publish SPM Package - XCFramework

---
+++ shared/src/commonMain/kotlin/com/santimattius/kvs/internal/InMemoryInstaces.kt
@@ -0,0 +1,35 @@
+package com.santimattius.kvs.internal
+
+import com.santimattius.kvs.internal.InMemoryKvs
+import kotlinx.collections.immutable.PersistentMap
+import kotlinx.collections.immutable.persistentMapOf
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.SynchronizedObject
+import kotlinx.coroutines.internal.synchronized
+import kotlin.concurrent.atomics.AtomicReference
+import kotlin.concurrent.atomics.ExperimentalAtomicApi
+
+// Cache for InMemoryKvs instances
+@OptIn(ExperimentalAtomicApi::class)
+private val inMemoryInstances =
+    AtomicReference<PersistentMap<String, InMemoryKvs>>(persistentMapOf())
+
+// Lock object for synchronization
+@OptIn(InternalCoroutinesApi::class)
+private val lock = SynchronizedObject()
+
+
+@OptIn(InternalCoroutinesApi::class, ExperimentalAtomicApi::class)
+internal fun provideInMemoryKvsInstance(name: String): InMemoryKvs {
+    val persistentMap = inMemoryInstances.load()
+    persistentMap[name]?.let { return it } // Atomic read
+
+    return synchronized(lock) {
+        val persistentMap = inMemoryInstances.load()
+        // Double-check locking pattern
+        persistentMap[name]?.let { return it }
+        val newDataStore = InMemoryKvs()
+        inMemoryInstances.store(persistentMap.put(name, newDataStore))
+        newDataStore
+    }
+}
