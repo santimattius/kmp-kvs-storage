580708de7437ffb456f856f2c1ccffa0b3e81631
GitHub Action <action@github.com>
2025-09-13 15:24:41 +0000

    Update Package.swift for version 1.0.0-ALPHA07

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KvsStorage",
-            url: "https://github.com/santimattius/kmp-kvs-storage/releases/download/1.0.0-ALPHA06/KvsStorage-1.0.0-ALPHA06.xcframework.zip",
-            checksum: "afd08b68b1b57a16988e2e4c32b42b186914dd7467592da1a98e6fc487a72014"
+            url: "https://github.com/santimattius/kmp-kvs-storage/releases/download/1.0.0-ALPHA07/KvsStorage-1.0.0-ALPHA07.xcframework.zip",
+            checksum: "c02b7ede36cdb922e3de81637adcd6f3d3ea5e26a2cf255baf4edaafebcd881f"
         )
     ]
 )
