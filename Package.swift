80aec151f26ef109b18e16c108e26aedc1eefe65
Santiago Mattiauda <santimattius@gmail.com>
2025-09-13 13:06:36 -0300

    Release/1.0.0 alpha09 (#3)
    * bump version
    
    * fix regex
    
    * [release] 1.0.0-ALPHA06
    
    * [release] 1.0.0-ALPHA06
    
    * [release] 1.0.0-ALPHA06
    
    * Update to version 1.0.0-ALPHA06
    
    * 1.0.0-ALPHA07
    
    * 1.0.0-ALPHA08
    
    * 1.0.0-ALPHA09
    
    * Update Package.swift for version 1.0.0-ALPHA09
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- Package.swift
+++ Package.swift
@@ -7,13 +7,16 @@ let package = Package(
         .iOS(.v14),
     ],
     products: [
-        .library(name: "KvsStorage", targets: ["KvsStorage"])
+        .library(
+            name: "KvsStorage",
+            targets: ["KvsStorage"]
+        ),
     ],
     targets: [
         .binaryTarget(
             name: "KvsStorage",
-            url: "https://github.com/santimattius/kmp-kvs-storage/releases/download/1.0.0-ALPHA02/KvsStorage.xcframework.zip",
-            checksum:"6f0b2832759d7d3d2c97a7efc879a894fbca533e4eb3053809b2f17130b242e6")
+            url: "https://github.com/santimattius/kmp-kvs-storage/releases/download/1.0.0-ALPHA09/KvsStorage-1.0.0-ALPHA09.xcframework.zip",
+            checksum: "22152227e19cac554d8a3d864475fb1e50fbb757cfcd63d4064e5f7117e3a9f8"
+        )
     ]
 )
-
