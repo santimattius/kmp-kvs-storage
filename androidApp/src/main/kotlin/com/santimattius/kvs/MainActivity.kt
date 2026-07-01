622f390780dac3215f6e0ebcb4aaef43933222ed
Santiago Mattiauda <santimattius@gmail.com>
2026-07-01 20:02:30 -0300

    Wrap UI in Scaffold and enable IME resize
    Add android:windowSoftInputMode="adjustResize" to AndroidManifest to let the UI resize when the keyboard appears. In MainActivity, wrap AppNavigation in a Scaffold and a full-size Box that applies the Scaffold content padding so composables respect system insets; add required Compose imports. Move enableEdgeToEdge call to after super.onCreate and enable @Preview to show system UI.

--- androidApp/src/main/kotlin/com/santimattius/kvs/MainActivity.kt
+++ androidApp/src/main/kotlin/com/santimattius/kvs/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.santimattius.kvs.navigation.AppNavigation
 import com.santimattius.kvs.ui.theme.AppTheme
@@ -12,18 +17,24 @@ import com.santimattius.kvs.ui.theme.AppTheme
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
             AppTheme {
-                AppNavigation()
+                Scaffold() {
+                    Box(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it)) {
+                        AppNavigation()
+                    }
+                }
             }
         }
     }
 }
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 fun AppNavigationPreview() {
     AppTheme {
